98f1a3dcb450c37a22456b27c4aa61f628ac394d
switer <guankaishe@gmail.com>
2013-11-29 13:47:44 +0800

    add route rule match interface

--- index.js
+++
@@ -1,6 +0,0 @@
-var proxyServer = require('./server');
-
-proxyServer.proxy([
-    { from: '10.1.84.165:4836', to: '10.1.84.68:9394', /*context:['GET:/post','/','!/public']*, '^/'*/ },
-    { from: 'localhost', to: '10.1.84.68:9394', /*context:[]*/ }
-]);
--- server.js
+++ server.js
@@ -1,19 +1,40 @@
-var httpProxy = require('http-proxy'),
-    colors = require('colors'),
-    _ = require('underscore');
+/**
+ *   Proxy server module
+ */
+
+var httpProxy = require('http-proxy'), // base on nodejitsu proxy server
+    colors = require('colors'), // use to pretty console log
+    _ = require('underscore'); // use each method.....
 
 var server = {
 
     /**
+     *   id for each proxy server
+     */
+    serverId: 1,
+    /**
      *   mapping root
      */
-    rules: {
-
-    },
+    rules: {},
+    /**
+     *   marked proxy host
+     */
+    proxies: {},
+    /**
+     *   a server to proxies indexes
+     */
+    serverProxiesIndexes: {},
     /**
      *   defualt port when config port is not pass in
      */
     defaultPort: 80,
+    /**
+     *   module regexps
+     */
+    regexps: {
+        // validate host string
+        HOST: /^\s*[0-9a-zA-Z\.]+\:?[0-9]*\s*$/,
+    },
     /**
      *   @param hosts[
      *                  {from: "0.0.0.0:8088", to: "github.com"},
@@ -21,18 +42,37 @@ var server = {
      *               ]
      */
     proxy: function (hosts) {
-        var hostsArray = this.parseHosts(hosts),
-            _this = this;
+        var _this = this;
 
-        _.each(hostsArray, function (hostConfig) {
-            _this.create(hostConfig.from, hostConfig.to);
+        _.each(hosts, function (hostConfig) {
+
+            var hostObj = {
+                from: _this.parseHost(hostConfig.from),
+                to: _this.parseHost(hostConfig.to)
+            }
+
+            var serverId = _this.create(hostObj.from, hostObj.to);
+
+            _this.saveHost(hostConfig, serverId);
+
         });
     },
     /**
      *   create a server for the one proxy config
      */
     create: function (from, to) {
+
+        var serverId = this.serverId, // for serverid hoisting
+
+            _this = this; // for hoisting context of `this`
+
         var server = httpProxy.createServer(function (req, res, proxy) {
+
+            var url = req.url;
+
+            var proxyKey = _this.serverProxiesIndexes[serverId],
+                proxyConfig = _this.proxies[proxyKey];
+
             proxy.proxyRequest(req, res, {
                 host: to.hostname,
                 port: to.port
@@ -45,23 +85,25 @@ var server = {
 
         console.log('Listen -> ' + from.hostname.green.grey + ' : ' + from.port.toString().blue.grey + 
                     ' forward ' + to.hostname.green.grey + ' : ' + to.port.toString().blue.grey);
+
+        return this.serverId ++;
     },
     /**
      *   rerturn the hostname + port format array
      */
-    parseHosts: function (hosts) {
-        var parseResult = [],
-            _this = this;
+    // parseHosts: function (hosts) {
+    //     var parseResult = [],
+    //         _this = this;
 
-        _.each(hosts, function (item) {
-            parseResult.push({
-                from: _this.parseHost(item.from),
-                to: _this.parseHost(item.to),
-            });
-        });
+    //     _.each(hosts, function (item) {
+    //         parseResult.push({
+    //             from: _this.parseHost(item.from),
+    //             to: _this.parseHost(item.to),
+    //         });
+    //     });
 
-        return parseResult;
-    },
+    //     return parseResult;
+    // },
     /**
      *   host is a string, should be parse to hostname + port object format
      */
@@ -73,6 +115,41 @@ var server = {
             hostname: hostname,
             port: port ? parseInt(port) : this.defaultPort
         };
+    },
+    /**
+     *   save each host config
+     */
+    saveHost: function (config, serverId) {
+
+        // craete host proxy config key
+        var proxyKey = [config.from, config.to].join('->');
+
+        // save proxy config
+        this.proxies[proxyKey] = {
+
+            from: this.parseHost(config.from),
+            to: this.parseHost(config.to),
+            rules: this.parseRouteRule(config.route)
+            
+        };
+
+        // serverid to proxies key indexes
+        this.serverProxiesIndexes[serverId] = proxyKey;
+    },
+    /**
+     *   validate host string
+     */
+    validHost: function (host) {
+        return this.regexps.HOST.exec(host) ? true : false;
+    },
+    /**
+     *   parse each route rule to url matched rule
+     */
+    parseRouteRule: function (routes) {
+        var _this = this;
+        _.each(routes, function (route) {
+
+        });
     }
 }
 
