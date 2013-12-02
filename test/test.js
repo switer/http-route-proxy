8793e4381f2b0feb1898e53d3e5c357c309de124
switer <guankaishe@gmail.com>
2013-12-02 19:37:18 +0800

    update proxy test file

--- test/test.js
+++ test/test.js
@@ -1,15 +1,16 @@
-var proxyServer = require('../server');
+var proxyServer = require('http-route-proxy');
 
 proxyServer.proxy([
-
+    // common
     {
-        from: '10.1.84.165:4836',
+        from: 'localhost:9000',
         to: 'dev7.ucweb.local:8183',
         route: ['/', '!/public', '!/test']
-    },{
-        from: 'localhost:4836',
-        // to: 'dev7.ucweb.local:8183',
-        to: 'dynamic.12306.cn',
+    },
+    // 80 port
+    {
+        from: 'localhost:9001',
+        to: 'github.io',
         route: ['/']
     }
 ]);
