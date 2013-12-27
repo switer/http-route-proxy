c95021c751ebb8d81555058aae676e3a8ff2642d
switer <guankaishe@gmail.com>
2013-12-27 14:27:57 +0800

    update test file;

--- test/https.js
+++ test/https.js
@@ -3,13 +3,15 @@ var httpProxy = require('http-proxy');
 var options = {
     changeOrigin: true,
     target: {
-        https: true
+        https: true,
+        rejectUnauthorized: false
     }
 }
 
+// process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = '0';
 httpProxy.createServer(options, function (req, res, proxy) {
     proxy.proxyRequest(req, res, {
-        host: 'github.com',
+        host: 'www.12306.cn',
         port: 443
     });
 }).listen(9001);
